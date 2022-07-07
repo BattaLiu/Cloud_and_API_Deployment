3c67243154f3e5f6c2d84631b784fd1784008b39
Jiayi Liu <821880891@qq.com>
2022-07-06 20:10:29 -0700

    Change battapp.py to create API endpoint (jsonify)

--- battapp.py
+++ battapp.py
@@ -5,8 +5,8 @@ Created on Tue Jun 28 00:49:58 2022
 
 @author: batta
 """
-import numpy as np
-from flask import Flask, request,render_template
+import pandas as pd
+from flask import Flask, jsonify, request
 import pickle
 
 app = Flask(__name__)
@@ -14,21 +14,23 @@ model = pickle.load(open('knn_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('characteristics.html')
+    data = "Hello world!"
+    return jsonify({'data':data})
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['GET','POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    age = request.args.get('age')
+    estimated_salary = request.args.get('estimated_salary')
+    test_df = pd.DataFrame({'Age': [age], 'Income': [estimated_salary]})
+    prediction = model.predict(test_df)
     def int_to_word(decision):
         word_dict = {0:"Not buy", 1:"Buy"}
         return word_dict[decision]
     output = int_to_word(prediction[0])
-    return render_template('characteristics.html', prediction_text='Purchase decision will be {}'.format(output))
+    return jsonify({'Purchase Decision:': output})
 
 if __name__ == "__main__":
     app.run(port = 3232,debug=True)
